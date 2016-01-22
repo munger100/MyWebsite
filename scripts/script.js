7eb77713157a72105df12167b536172ec2cf9f31
Matthew Munger <matthew.munger@outlook.com>
2016-01-22 14:05:08 -0400

    Added custom fonts, still have references to root and not folder navigation specific to each page, navbar slightly improved

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,23 @@
-if document
+$(document).ready(function () {
+    var body = $(document.getElementsByTagName('body')),
+        navbar = '<div class="navbar">' +
+            '<ul> ' +
+            '<li class="navbarli"">' +
+            '<a href="/MyWebsite/index.html">HOME PAGE</a>' +
+            '</li> ' +
+            '<li class="navbarli">' +
+            '<a href="/MyWebsite/subpages/about.html">ABOUT ME</a>' +
+            '</li> ' +
+            '<li class="navbarli" onclick="window.location.href=\'/MyWebsite/subpages/contact.html\'">' +
+            '<a href="/MyWebsite/subpages/contact.html">CONTACT ME</a>' +
+            '</li>' +
+            '</ul>' +
+            '</div>',
+        footer = '<div class="footer" align="center">' +
+            'Matthew Munger &#9731;' +
+            '</div>',
+        reset_float = '<div style="clear: both"></div>';
+    // Insert navbar and footer
+    body.html(navbar + reset_float + body.html() + reset_float + footer);
+
+});
