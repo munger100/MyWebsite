3feb64587930df69a14d4b2e2f5c9ee46a30ba6d
Matthew Munger <matthew.munger@outlook.com>
2016-01-29 22:25:42 -0400

    First Submission, done.

--- js/script.js
+++ js/script.js
@@ -1,25 +1,25 @@
 $(document).ready(function () {
-    var indexs =  window.location.href.split("/");
-    var index =indexs[indexs.length-1] == "index.html";
+    var indexs = window.location.href.split("/");
+    var index = indexs[indexs.length - 1] == "index.html";
     var body = $(document.getElementsByTagName('body')),
         navbar = '<div class="navbar">' +
             '<ul> ' +
             '<li class="navbarli"">' +
-            '<a href="' + (index ? "" : "../" )+ 'index.html">HOME PAGE</a>' +
+            '<a href="' + (index ? "" : "../" ) + 'index.html">HOME PAGE</a>' +
             '</li> ' +
             '<li class="navbarli">' +
-            '<a href="' + (index ? "" : "../" )+ 'subpages/about.html">ABOUT ME</a>' +
+            '<a href="' + (index ? "" : "../" ) + 'subpages/about.html">ABOUT ME</a>' +
             '</li> ' +
             '<li class="navbarli">' +
-            '<a href="' + (index ? "" : "../" )+ 'subpages/contact.html">CONTACT ME</a>' +
+            '<a href="' + (index ? "" : "../" ) + 'subpages/contact.html">CONTACT ME</a>' +
             '</li>' +
             '</ul>' +
             '</div>',
-        footer = '<div class="footer" align="center">' +
+        footer = '<div id="footer" align="center">' +
             'Matthew Munger &#9731;' +
             '</div>',
         reset_float = '<div style="clear: both"></div>';
     // Insert navbar and footer
-    body.html(navbar + reset_float + body.html() + reset_float + footer);
+    body.html('<div id="wrapper">' + navbar + reset_float + body.html() + reset_float + footer + '</div>');
 
 });
